39b46bbb39f9580b534e4160211ef8fce20bb0e1
joldasovmuhammed <mukhammed397@gmail.com>
2025-02-26 09:52:03 +0500

    java-script simple math

---
+++ script.js
@@ -0,0 +1,15 @@
+// // // let user = "Muhammed"
+// // const user = "Muhammed"
+// // user = "Abbaz"
+// // console.log(user);
+// // let userAge = 19;
+// // userAge = 20;
+// "use strict"
+// azamat = 22;
+// console.log(azamat);
+let a = 10;
+const b = 20;
+console.log(a*b);
+a = 50;
+console.log(a/b);
+
